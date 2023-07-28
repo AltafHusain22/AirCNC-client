8200f5011aa43ae48dd41eb1ea17e70aa4ee048d
AltafHusain22 <fiverr.contactme@gmail.com>
2023-07-28 10:37:08 +0600

    search box added

--- src/components/Shared/NavBar/MainNav.jsx
+++ src/components/Shared/NavBar/MainNav.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Container from '../Container/Container';
 import Logo from './Logo';
+import Search from './Search';
 
 const MainNav = () => {
 	return (
-		<div className='h-[60px] bg-white shadow-sm fixed w-full z-10'>
+		<div className='h-[90px] bg-white shadow-sm fixed w-full z-10'>
 			<div className='py-4  '>
 				<Container> 
 					<div className='flex justify-between items-center gap-3 md:gap-0'>
 						<Logo></Logo>
-						<div>Menu</div>
+						<Search></Search>
 						<div>avatar</div>
 					</div>
 				</Container>
